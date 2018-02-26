3d7333646eab0d8f0f17ed467237e0a68a4d4a5c
Iain Dooley <iain@workingsoftware.com.au>
2018-02-26 22:00:48 +1100

    Fixed test for new API

--- Board.js.murphy/default.run.js
+++ Board.js.murphy/default.run.js
@@ -3,7 +3,7 @@ eval(murphy.load(__dirname,"../Board.js"));
 ////////////////////////////////////////////////////////////////////////////
 
 Board({id:"87asdfadf"})
-.moveCards({from:"One",to:"Two"})
-.findCard({list:"Two",name:/Cards in ".+"/})
-.move({to:"One",position:"top"});
+.moveAllCards({from:"One",to:"Two"});
 
+Board({id:"87asdfadf"})
+.list({name:"One",to:"Two"});
