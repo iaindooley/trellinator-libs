d12a7eeb80fb70a3b2d839d388beaa264a6f2216
Iain Dooley <iain@workingsoftware.com.au>
2018-02-27 10:02:48 +1100

    Tmp parse error fix

--- Board.js
+++ Board.js
@@ -5,9 +5,9 @@ var Board = function(data)
     this.moveAllCards = function(data)
     {
         //data.from == RegExp, data.to == RegExp
-        var lists = TrelloApi("key","token").get("boards/"+this.data.id+"/lists/open");
+        var lists = TrelloApi().get("boards/"+this.data.id+"/lists/open");
 
-https://api.trello.com/1/boards/nmWXD05b/lists/open?key=fbd7dbac07f0b9476434fa48cf6cb2dd&token=dc1224ae737e40c1e2ffb53195c28942b3c5399a8916b48796120a4e68f4ee60'
+//https://api.trello.com/1/boards/nmWXD05b/lists/open?key=fbd7dbac07f0b9476434fa48cf6cb2dd&token=dc1224ae737e40c1e2ffb53195c28942b3c5399a8916b48796120a4e68f4ee60'
     }
 
     this.list = function()
