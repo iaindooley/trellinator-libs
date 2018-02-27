1df8768c2ccfccc1a45cb24ab1aa508316231c7f
Iain Dooley <iain@workingsoftware.com.au>
2018-02-27 13:20:22 +1100

    Implemented initial test with proper fixture

--- TrelloApi.js.murphy/default.run.js
+++ TrelloApi.js.murphy/default.run.js
@@ -1,20 +1,13 @@
 //////////////////////////////////////////////////////
-const path = require("path");
-const fs = require("fs");
 const murphy = require("murphytest");
-const glob = require("glob")
-const {exec} = require('child_process');
-const cmd = "node ";
 //////////////////////////////////////////////////////
 eval(murphy.load(__dirname,"../TrelloApi.js"));
 eval(murphy.load(__dirname,"../TestConnector.js"));
 //////////////////////////////////////////////////////
-TestConnector.path = __dirname;
+TestConnector.test_base_dir   = __dirname;
 
-var actual = TrelloApi().post("cards/9878a9sdadf/idLabels?value=878asdfdf");
+var actual = TrelloApi.post("boards/nmWXD05b/lists?name="+encodeURIComponent("New List")+"&pos=top").idBoard;
+var expected = "5a938de4e0c2896bd94c7434";
 
-var expected = "{\"method\":\"post\",\"muteHttpExceptions\":true}\nhttps://trello.com/1/cards/9878a9sdadf/idLabels?value=878asdfdf&key=key&token=token\n";
-    
 if(actual != expected)
-    console.log("Unexpected output from TrelloApi in TrelloApi.js.murphy/default.run.js: \n"+actual);
-
+    console.log("Got unexpected board id: "+actual);
