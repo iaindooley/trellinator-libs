bed54cb8ae8adc1efcad3ba9c6ccc9c0ad814908
Iain Dooley <iain@workingsoftware.com.au>
2018-02-27 13:19:12 +1100

    Fixed fixture path issues, and made entirely static

--- TrelloApi.js
+++ TrelloApi.js
@@ -1,76 +1,72 @@
-var TrelloApi = function()
+var TrelloApi = function(){};
+TrelloApi.post = function(baseURL)
 {
-    this.post = function(baseURL)
+    return TrelloApi.call("post",baseURL);
+}
+
+TrelloApi.get = function(baseURL)
+{
+    return TrelloApi.call("get",baseURL);
+}
+
+TrelloApi.del = function(baseURL)
+{
+    return TrelloApi.call("get",baseURL);
+}
+
+TrelloApi.put = function(baseURL)
+{
+    return TrelloApi.call("get",baseURL);
+}
+
+TrelloApi.call = function(method,baseURL)
+{
+    var url = TrelloApi.constructTrelloURL(baseURL);
+    var connector = (typeof UrlFetchApp == "undefined")? TestConnector():UrlFetchApp;
+    var resp = connector.fetch(url, {"method": method,"muteHttpExceptions":true});
+    
+    if(typeof Utilities != "undefined")
+        Utilities.sleep(5);
+
+    return JSON.parse(resp);
+}
+
+TrelloApi.constructTrelloURL = function(baseURL)
+{
+    var freshURL = "";
+    var creds = {key: "dummy",token: "dummy"};
+
+    try
     {
-        return this.call("post",baseURL);
+        creds = TrelloApi.checkControlValues();
+    }
+    
+    catch(e)
+    {
     }
 
-    this.get = function(baseURL)
-    {
-        return this.call("get",baseURL);
-    }
+    if (baseURL.indexOf("?") == -1)
+        freshURL = "https://api.trello.com/1/"+ baseURL +"?key="+ creds.key + "&token="+ creds.token;
+    else
+        freshURL = "https://api.trello.com/1/"+ baseURL +"&key="+ creds.key +"&token="+ creds.token;
+    
+    return freshURL;
+}
 
-    this.del = function(baseURL)
-    {
-        return this.call("get",baseURL);
-    }
-
-    this.put = function(baseURL)
-    {
-        return this.call("get",baseURL);
-    }
-
-    this.call = function(method,baseURL)
-    {
-        var url = this.constructTrelloURL(baseURL);
-        var connector = (typeof UrlFetchApp == "undefined")? TestConnector():UrlFetchApp;
-        var resp = connector.fetch(url, {"method": method,"muteHttpExceptions":true});
-        
-        if(typeof Utilities != "undefined")
-            Utilities.sleep(5);
-
-        return resp;
-    }
-
-    this.constructTrelloURL = function(baseURL)
-    {
-        var freshURL = "";
-        var creds = {key: "dummy",token: "dummy"};
-
-        try
-        {
-            creds = this.checkControlValues();
-        }
-        
-        catch(e)
-        {
-        }
-
-        if (baseURL.indexOf("?") == -1)
-            freshURL = "https://trello.com/1/"+ baseURL +"?key="+ creds.key + "&token="+ creds.token;
-        else
-            freshURL = "https://trello.com/1/"+ baseURL +"&key="+ creds.key +"&token="+ creds.token;
-        
-        return freshURL;
-    }
-
-    this.checkControlValues = function()
-    { 
-        var col = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(CONFIG_NAME).getRange("B2:B3").getValues();
+TrelloApi.checkControlValues = function()
+{ 
+    var col = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(CONFIG_NAME).getRange("B2:B3").getValues();
  
-        var appKey = (col[0][0] + "").trim();
+    var appKey = (col[0][0] + "").trim();
  
-        if(appKey == "")
-            return {key: "", token: "", err: "Trello Key not found in " + CONFIG_NAME + " tab." };
+    if(appKey == "")
+        return {key: "", token: "", err: "Trello Key not found in " + CONFIG_NAME + " tab." };
    
-        var token = (col[1][0] + "").trim();
+    var token = (col[1][0] + "").trim();
 
-        if(token == "")
-            return {key: "", token: "", err: "Trello Token not found in " + CONFIG_NAME + " tab." };
+    if(token == "")
+        return {key: "", token: "", err: "Trello Token not found in " + CONFIG_NAME + " tab." };
 
-        //both found
-        return {key: appKey, token: token, err:""};
-    }
-
-    return this;
+    //both found
+    return {key: appKey, token: token, err:""};
 }
