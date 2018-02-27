f983acb24f421a18f166773092f26de9107385be
Iain Dooley <iain@workingsoftware.com.au>
2018-02-27 14:26:43 +1100

    Implemented moveAllCards

--- Board.js
+++ Board.js
@@ -5,15 +5,25 @@ var Board = function(data)
     this.moveAllCards = function(data)
     {
         //data.from == RegExp, data.to == RegExp
-        var lists = TrelloApi.get("boards/"+this.data.id+"/lists/open");
+        var lists = TrelloApi.get("boards/"+this.data.id+"/lists?filter=open&fields=all");
+        var from_list = null;
+        var to_list = null;
 
-//https://api.trello.com/1/boards/nmWXD05b/lists/open?key=fbd7dbac07f0b9476434fa48cf6cb2dd&token=dc1224ae737e40c1e2ffb53195c28942b3c5399a8916b48796120a4e68f4ee60'
+        for(var key in lists)
+        {
+            if(TrelloApi.nameTest(data.from,lists[key].name))
+                from_list = lists[key];
+            else if(TrelloApi.nameTest(data.to,lists[key].name))
+                to_list = lists[key];
+        }
+        
+        return TrelloApi.post("lists/"+from_list.id+"/moveAllCards?idBoard="+to_list.idBoard+"&idList="+to_list.id);
     }
 
     this.list = function()
     {
         //{name: new RegExp(list_name+" \\([0-9]+\\)")});
     }
-    
+
     return this;
 }
