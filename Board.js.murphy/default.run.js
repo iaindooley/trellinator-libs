239dd9ba4ff19c125318ef72180dc08c0f88c7e9
Iain Dooley <iain@workingsoftware.com.au>
2018-02-27 13:19:25 +1100

    Adjusted for new testing system

--- Board.js.murphy/default.run.js
+++ Board.js.murphy/default.run.js
@@ -1,5 +1,7 @@
 const murphy = require("murphytest");
 eval(murphy.load(__dirname,"../Board.js"));
+eval(murphy.load(__dirname,"../TrelloApi.js"));
+eval(murphy.load(__dirname,"../TestConnector.js"));
 ////////////////////////////////////////////////////////////////////////////
 
 Board({id:"87asdfadf"})
