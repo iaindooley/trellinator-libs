6c0fa9ea2f22714f923d0198fca707f55fec6158
Iain Dooley <iain@workingsoftware.com.au>
2018-02-28 10:03:23 +1100

    Added initial checklist implementation and tests

---
+++ Checklist.js.murphy/default.run.js
@@ -0,0 +1,9 @@
+const murphy = require("murphytest");
+eval(murphy.load(__dirname,"../Checklist.js"));
+eval(murphy.load(__dirname,"../TrelloApi.js"));
+eval(murphy.load(__dirname,"../TestConnector.js"));
+eval(murphy.load(__dirname,"../IterableCollection.js"));
+TestConnector.test_base_dir = __dirname;
+
+if(Checklist({id: "5a95df4138a1ec239ce5d520"}).addItem("Checklist item one").added_item.id != "5a95e093795a022797099c36")
+    console.log("Got back incorrect id from added checklist item");
