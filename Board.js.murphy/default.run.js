4e27de05f6535512d20219bf6bfce81ad6e92dd1
Iain Dooley <iain@workingsoftware.com.au>
2018-02-27 14:27:30 +1100

    Added test for moveAllCards with real fixtures

--- Board.js.murphy/default.run.js
+++ Board.js.murphy/default.run.js
@@ -3,9 +3,8 @@ eval(murphy.load(__dirname,"../Board.js"));
 eval(murphy.load(__dirname,"../TrelloApi.js"));
 eval(murphy.load(__dirname,"../TestConnector.js"));
 ////////////////////////////////////////////////////////////////////////////
+TestConnector.test_base_dir   = __dirname;
 
-Board({id:"87asdfadf"})
-.moveAllCards({from:"One",to:"Two"});
+if(Object.keys(Board({id:"5a938de4e0c2896bd94c7434"}).moveAllCards({from:"Test from",to:"Test to"})).length < 3)
+    console.log("Unexpected number of elements returned during move all cards operation");
 
-Board({id:"87asdfadf"})
-.list({name:"One",to:"Two"});
