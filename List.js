f1683a7b9a172674a38852257309f87f1826365f
Iain Dooley <iain@workingsoftware.com.au>
2018-03-07 09:52:00 +1100

    Only fetch card id in fields to minimise size of response

--- List.js
+++ List.js
@@ -15,7 +15,7 @@ var List = function(data)
     {
         if(!this.cards)
         {
-            this.cards = new IterableCollection(TrelloApi.get("lists/"+this.data.id+"/cards"));
+            this.cards = new IterableCollection(TrelloApi.get("lists/"+this.data.id+"/cards?fields=id"));
             
             this.cards.transform(function(elem)
             {
