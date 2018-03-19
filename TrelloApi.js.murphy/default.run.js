7f464e765db31552be9209fed6adcd3dfd05d21a
Iain Dooley <iain@workingsoftware.com.au>
2018-03-19 21:02:26 +1100

    Updated to get live key and token from argv

--- TrelloApi.js.murphy/default.run.js
+++ TrelloApi.js.murphy/default.run.js
@@ -1,10 +1,13 @@
-//////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 const murphy = require("murphytest");
 //////////////////////////////////////////////////////
 eval(murphy.load(__dirname,"../TrelloApi.js"));
 eval(murphy.load(__dirname,"../TestConnector.js"));
+eval(murphy.load(__dirname,"../IterableCollection.js"));
 //////////////////////////////////////////////////////
 TestConnector.test_base_dir   = __dirname;
+TestConnector.live_key      = process.argv[2];
+TestConnector.live_token    = process.argv[3];
 
 var actual = TrelloApi.post("boards/nmWXD05b/lists?name="+encodeURIComponent("New List")+"&pos=top").idBoard;
 var expected = "5a938de4e0c2896bd94c7434";
