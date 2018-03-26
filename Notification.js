bc7ece2384972b682690bb79d4f298226d24e1e6
Iain Dooley <iain@workingsoftware.com.au>
2018-03-26 14:07:56 +1100

    Added methods for dealing with checklist notifications

--- Notification.js
+++ Notification.js
@@ -2,6 +2,37 @@ var Notification = function(notification)
 {
     this.notification = notification;
     
+    this.completedChecklistItem = function(name)
+    {
+        if(this.notification.action.display.translationKey != "action_completed_checkitem")
+            throw new Error("No checklist item was completed");
+
+        var ret = new CheckItem(this.notification.action.display.entities.checkitem);
+        ret.setContainingChecklist(this.checklist);
+        return ret;
+    }
+
+    this.checklist = function()
+    {
+        return new Checklist(this.notification.action.data.checklist);
+    }
+
+    this.completedChecklist = function(name)
+    {
+        if(this.notification.action.display.translationKey != "action_completed_checkitem")
+            throw new Error("No checklist item was completed, therefore no checklist was completed as part of this action");
+
+        var ret = this.checklist();
+        
+        if(!ret.isComplete())
+            throw new Error("The checklist in which the item was checked is not complete");
+        else if(name && !TrelloApi.nameTest(name,ret.name()))
+            throw new Error("The completed checklist was not named "+name);
+        
+        ret.setContainingCard(this.card());
+        return ret;
+    }
+
     this.listCardWasAddedTo = function()
     {
         if(new IterableCollection(["action_create_card","action_copy_card","action_email_card"]).hasMember(this.notification.action.display.translationKey))
