0768ec520ef3385010207093a3be2dccbd505fa1
Iain Dooley <iain@workingsoftware.com.au>
2018-03-26 13:25:47 +1100

    Added new class for CheckItem

---
+++ CheckItem.js
@@ -0,0 +1,32 @@
+var CheckItem = function(data)
+{
+    this.data                 = data;
+    this.containing_checklist = null;
+
+    this.setContainingChecklist = function(checklist)
+    {
+        this.containing_checklist = checklist;
+    }
+
+    this.url = function()
+    {
+        if(!this.data.url)
+            this.load();
+        
+        return this.data.url;
+    }
+
+    this.name = function()
+    {
+        if(!this.data.nameHtml && !this.data.name)
+            this.load();
+        
+        return (this.data.name) ? this.data.name:this.data.nameHtml;
+    }
+
+    this.load = function()
+    {
+        this.data = TrelloApi.get("checklists/"+this.containing_checklist.data.id+"/checkitems/"+this.data.id+"?fields=all");
+        return this;
+    }
+}
