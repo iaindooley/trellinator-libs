0dd53708b6326b7d64561a18c7c2c7d772217700
Iain Dooley <iain@workingsoftware.com.au>
2018-04-06 13:56:18 +1000

    Moved the nameTestData function to TrelloApi

--- Board.js
+++ Board.js
@@ -81,7 +81,7 @@ var Board = function(data)
 
     this.list = function(data)
     {
-        return this.lists().findByName(Board.nameTestData(data)).first();
+        return this.lists().findByName(TrelloApi.nameTestData(data)).first();
     }
 
     this.lists = function(data)
@@ -95,7 +95,7 @@ var Board = function(data)
                                  });
         }
       
-        if(filter = Board.nameTestData(data))
+        if(filter = TrelloApi.nameTestData(data))
             this.list_of_lists.filterByName(filter);
 
         return this.list_of_lists;
@@ -166,16 +166,4 @@ var Board = function(data)
     {
         this.data = TrelloApi.get("boards/"+this.data.id+"?actions=none&boardStars=none&cards=none&checklists=none&fields=name%2C%20desc%2C%20descData%2C%20closed%2C%20idOrganization%2C%20url%2C%20shortUrl&lists=none&members=none&memberships=none&membersInvited=none");
     }
-}
-
-Board.nameTestData = function(data)
-{
-    var ret = null;
-
-    if((typeof data == "string") || ((typeof data !== "undefined") && (data.constructor === RegExp)))
-        ret = data;
-    else if(data && data.name)
-        ret = data.name;
-    
-    return ret;
 }
