c16e01240f66ec84394e1bb0bc545b5467b0fc13
Iain Dooley <iain@workingsoftware.com.au>
2018-04-23 20:07:49 +1000

    Split TrelloApi into 2 classes so that HttpApi can be used generically, eg. for pipedrive API

---
+++ HttpApi.js
@@ -0,0 +1,50 @@
+var HttpApi = function(){};
+HttpApi.call = function(method,url,force_get)
+{
+    var payload = null;
+    var params  = {"method": method,"muteHttpExceptions":true};
+
+    if((method == "post") || (method == "put"))
+    {
+        if (url.indexOf("?") != -1)
+        {
+            var parts         = url.split("?");
+            var url           = parts[0];
+            var payload_parts = parts[1].split("&");
+            var payload       = {};
+            
+            for(var key in payload_parts)
+            {
+              var sub_parts = payload_parts[key].split("=");
+              if(sub_parts[0] != force_get)
+                payload[sub_parts[0]] = decodeURIComponent(sub_parts[1]);
+              else
+                url += "?"+sub_parts[0]+"="+sub_parts[1];
+            }
+            
+            params.payload = payload;
+        }
+    }
+
+    var connector = (typeof UrlFetchApp == "undefined")? new TestConnector():UrlFetchApp;
+    var options
+    var resp = connector.fetch(url,params);
+
+
+    if(typeof Utilities != "undefined")
+        Utilities.sleep(50);
+    
+    var ret = null;
+  
+    try
+    {
+        ret = JSON.parse(resp);
+    }
+  
+    catch(e)
+    {
+       writeInfo_("Unable to parse response: "+resp+" from URL: "+url+" with method: "+method+". Got error: "+e);
+    }
+  
+    return ret;
+}
--- TrelloApi.js
+++ TrelloApi.js
@@ -8,70 +8,22 @@ TrelloApi.cardLinkRegExp = function()
 
 TrelloApi.post = function(baseURL)
 {
-    return TrelloApi.call("post",baseURL);
+    return HttpApi.call("post",TrelloApi.constructTrelloURL(baseURL));
 }
 
 TrelloApi.get = function(baseURL)
 {
-    return TrelloApi.call("get",baseURL);
+    return HttpApi.call("get",TrelloApi.constructTrelloURL(baseURL));
 }
 
 TrelloApi.del = function(baseURL)
 {
-    return TrelloApi.call("delete",baseURL);
+    return HttpApi.call("delete",TrelloApi.constructTrelloURL(baseURL));
 }
 
 TrelloApi.put = function(baseURL)
 {
-    return TrelloApi.call("put",baseURL);
-}
-
-TrelloApi.call = function(method,baseURL)
-{
-    var url     = TrelloApi.constructTrelloURL(baseURL);
-    var payload = null;
-    var params  = {"method": method,"muteHttpExceptions":true};
-
-    if((method == "post") || (method == "put"))
-    {
-        if (url.indexOf("?") != -1)
-        {
-            var parts         = url.split("?");
-            var url           = parts[0];
-            var payload_parts = parts[1].split("&");
-            var payload       = {};
-            
-            for(var key in payload_parts)
-            {
-                var sub_parts = payload_parts[key].split("=");
-                payload[sub_parts[0]] = decodeURIComponent(sub_parts[1]);
-            }
-            
-            params.payload = payload;
-        }
-    }
-
-    var connector = (typeof UrlFetchApp == "undefined")? new TestConnector():UrlFetchApp;
-    var options
-    var resp = connector.fetch(url,params);
-
-
-    if(typeof Utilities != "undefined")
-        Utilities.sleep(50);
-    
-    var ret = null;
-  
-    try
-    {
-        ret = JSON.parse(resp);
-    }
-  
-    catch(e)
-    {
-       writeInfo_("Unable to parse response: "+resp+" from URL: "+url+" with method: "+method+". Got error: "+e);
-    }
-  
-    return ret;
+    return HttpApi.call("put",TrelloApi.constructTrelloURL(baseURL));
 }
 
 TrelloApi.constructTrelloURL = function(baseURL)
