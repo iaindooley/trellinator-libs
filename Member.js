c8f6aa8363d4454171f30c4b7e73ff0c5ae0af68
Iain Dooley <iain@workingsoftware.com.au>
2018-06-15 13:26:54 +1000

    Fixed typo TrellApi

--- Member.js
+++ Member.js
@@ -143,7 +143,7 @@ var Member = function(data)
     {
         if(!this.board_list)
         {
-            this.board_list = new IterableCollection(TrellApi.get("members/"+this.username()+"/boards?filter=open&fields=all&lists=open&memberships=none&organization_fields=name%2CdisplayName")).transform(function(elem)
+            this.board_list = new IterableCollection(TrelloApi.get("members/"+this.username()+"/boards?filter=open&fields=all&lists=open&memberships=none&organization_fields=name%2CdisplayName")).transform(function(elem)
                               {
                                   return new Board(elem);
                               });
