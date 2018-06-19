0e12b476d6a2ec700dde248452e1df94118d14cb
Iain Dooley <iain@workingsoftware.com.au>
2018-06-19 18:41:56 +1000

    Added documentation

--- CheckItem.js
+++ CheckItem.js
@@ -1,7 +1,27 @@
 /**
 * @class CheckItem
 * @memberof module:TrelloEntities
+* @param data (Object} key/value pairs of 
+* information, must at least contain "id",
+* can basically just pass in response from Trello API
 * @constructor
+* @classdesc The CheckItem class represents
+* a checklist item in Trello. 
+*
+* You will typically only interact with this class
+* as a return value in an IterableCollection from
+* a checklist.
+*
+* @example
+* card.checklist("Something").item(new RegExp("Process.*")).remove();
+* @example
+* card.checklist("Something").item(new RegExp("Process.*")).mark("complete");
+* @example
+* card.checklist("Something").items().each(function(item)
+* {
+*     if(new RegExp("Start.*").test(item.name()))
+*         item.remove();
+* });
 */
 var CheckItem = function(data)
 {
@@ -9,10 +29,10 @@ var CheckItem = function(data)
     this.containing_checklist = null;
 
     /**
-    * Ohai there
+    * Return the id of this item
     * @memberof module:TrelloEntities.Card
     * @example
-    * new Notification(posted).board().id();
+    * card.checklist("Something").item("Blah").id();
     */
     this.id = function()
     {
@@ -20,22 +40,24 @@ var CheckItem = function(data)
     }
 
     /**
-    * Ohai there
+    * Remove this item from it's containing checklist
+    * and return the checklist
     * @memberof module:TrelloEntities.Card
     * @example
-    * new Notification(posted).board().id();
+    * card.checklist("Something").item("Blah").remove();
     */
     this.remove = function()
     {
         TrelloApi.del("cards/"+this.containing_checklist.card().data.id+"/checkItem/"+this.data.id);
+        this.containing_checklist.check_items = null;
         return this.containing_checklist;
     }
 
     /**
-    * Ohai there
+    * Return the containing Checklist
     * @memberof module:TrelloEntities.Card
     * @example
-    * new Notification(posted).board().id();
+    * card.checklist("Something").item("Blah").checklist().item("Blah");//AARRGH!
     */
     this.checklist = function()
     {
@@ -43,57 +65,40 @@ var CheckItem = function(data)
     }
 
     /**
-    * Ohai there
+    * Set the text for this item (ie. it's name)
     * @memberof module:TrelloEntities.Card
+    * @param name {string} the text to appear for the checklist item
     * @example
-    * new Notification(posted).board().id();
-    */
-    this.setContainingChecklist = function(checklist)
-    {
-        this.containing_checklist = checklist;
-        return this;
-    }
-
-    /**
-    * Ohai there
-    * @memberof module:TrelloEntities.Card
-    * @example
-    * new Notification(posted).board().id();
+    * card.checklist("Something").items().first().setName("Blah");
     */
     this.setName = function(name)
     {
         TrelloApi.put("cards/"+this.containing_checklist.card().data.id+"/checkItem/"+this.data.id+"?name="+encodeURIComponent(name));
+        this.data.name = name;
         return this;
     }
 
     /**
-    * Ohai there
+    * Change the state of this item to
+    * either complete or incomplete
     * @memberof module:TrelloEntities.Card
+    * @param state {string} either complete or incomplete
     * @example
-    * new Notification(posted).board().id();
-    */
-    this.url = function()
-    {
-        if(!this.data.url)
-            this.load();
-        
-        return this.data.url;
-    }
-
-    /**
-    * Ohai there
-    * @memberof module:TrelloEntities.Card
-    * @example
-    * new Notification(posted).board().id();
+    * card.checklist("Something").items().first().mark("complete");
     */
     this.mark = function(state)
     {
       TrelloApi.put("cards/"+this.checklist().card().data.id+"/checkItem/"+this.data.id+"?state="+state);
+      this.data.state = state;
       return this;
     }
 
     /**
+    * Return true of this is complete
     * @memberof module:TrelloEntities.Card
+    * @example
+    * if(card.checklist("Something").items().first().isComplete())
+    *     card.postComment("The first item in the Something checklist is complete");
     */
     this.isComplete = function()
     {
@@ -101,24 +106,10 @@ var CheckItem = function(data)
     }
 
     /**
-    * Ohai there
+    * Return the name (ie. full text) for this item
     * @memberof module:TrelloEntities.Card
     * @example
-    * new Notification(posted).board().id();
-    */
-    this.state = function()
-    {
-        if(!this.data.state)
-            this.load();
-        
-        return this.data.state;
-    }
-
-    /**
-    * Ohai there
-    * @memberof module:TrelloEntities.Card
-    * @example
-    * new Notification(posted).board().id();
+    * card.checklist("Something").items().first().name();
     */
     this.name = function()
     {
@@ -128,15 +119,35 @@ var CheckItem = function(data)
       return (this.data.text)?this.data.text:this.data.name;
     }
 
-    /**
-    * Ohai there
-    * @memberof module:TrelloEntities.Card
-    * @example
-    * new Notification(posted).board().id();
-    */
+    //INTERNAL USE ONLY
     this.load = function()
     {
         this.data = TrelloApi.get("checklists/"+this.containing_checklist.data.id+"/checkitems/"+this.data.id+"?fields=all");
         return this;
+    }
+
+    //INTERNAL USE ONLY
+    this.setContainingChecklist = function(checklist)
+    {
+        this.containing_checklist = checklist;
+        return this;
+    }
+
+    //DEPRECATED
+    this.url = function()
+    {
+        if(!this.data.url)
+            this.load();
+        
+        return this.data.url;
+    }
+
+    //DEPRECATED: use isComplete
+    this.state = function()
+    {
+        if(!this.data.state)
+            this.load();
+        
+        return this.data.state;
     }
 }
