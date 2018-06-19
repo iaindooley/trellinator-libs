031184dd63f51b8be6aa3cb33e0869d217f78be8
Iain Dooley <iain@workingsoftware.com.au>
2018-06-19 20:56:27 +1000

    Added documentation

--- Team.js
+++ Team.js
@@ -1,7 +1,18 @@
 /**
 * @class Team
 * @memberof module:TrelloEntities
+* @param data (Object} key/value pairs of 
+* information, must at least contain "id",
+* can basically just pass in response from Trello API
 * @constructor
+* @classdesc The Team class represents
+* a Team in Trello. 
+* 
+* You will mostly deal with this as a return value
+* from Member and Trellinator.
+*
+* @example
+* new Trellinator().team("New Or Existing Team");
 */
 var Team = function(data)
 {    
@@ -9,10 +20,10 @@ var Team = function(data)
     this.board_list = null;
 
     /**
-    * Ohai there
+    * Return the id of this Team
     * @memberof module:TrelloEntities.Team
     * @example
-    * new Notification(posted).board().id();
+    * new Trellinator().team("New Or Existing Team").id();
     */
     this.id = function()
     {
@@ -20,10 +31,10 @@ var Team = function(data)
     }
   
     /**
-    * Ohai there
+    * Return the name of this team
     * @memberof module:TrelloEntities.Team
     * @example
-    * new Notification(posted).board().id();
+    * new Trellinator().team("Name").name();
     */
     this.name = function()
     {
@@ -34,23 +45,33 @@ var Team = function(data)
     }
 
     /**
-    * Ohai there
+    * Return a board that is in this team
+    * filtered by name or regex, if more than
+    * one board matches the first will be returned
     * @memberof module:TrelloEntities.Team
+    * @param name {string|RegExp} the string or regex to match
+    * against the board names
     * @example
-    * new Notification(posted).board().id();
+    * new Trellinator().team("My Team").boards().each(function(board)
+    * {
+    *     board.list("ToDo").cards().first().postComment("Me first!");
+    * });
     */
-    this.board = function(data)
+    this.board = function(name)
     {
-        return this.boards(data).first();
+        return this.boards(name).first();
     }
 
     /**
-    * Ohai there
+    * Return an IterableCollection of boards that are 
+    * in this team, filtered by string/regex
     * @memberof module:TrelloEntities.Team
+    * @param name {string|RegExp} a string or regex to match
+    * against board names
     * @example
-    * new Notification(posted).board().id();
+    * new Trellinator().team("My Team").boards();
     */
-    this.boards = function(data)
+    this.boards = function(name)
     {
         if(!this.board_list)
         {
@@ -60,9 +81,18 @@ var Team = function(data)
             });
         }
 
-        return this.board_list.findByName(data);
+        return this.board_list.findByName(name);
     }
     
+    /**
+    * Add a member to this team, must be via
+    * email rather than by username (weirdly!)
+    * @memberof module:TrelloEntities.Team
+    * @param email {string} the email of the person to invite
+    * @param full_name {string} full name for the person
+    * @param type {string} either "admin" or "normal", optional, default normal
+    * @example
+    * new Trellinator().team("Some Team").addMember("team@theprocedurepeople.com","Cool Folks","admin");
     this.addMember = function(email,full_name,type)
     {
         if(!type)
@@ -72,12 +102,7 @@ var Team = function(data)
         return this;
     }
 
-    /**
-    * Ohai there
-    * @memberof module:TrelloEntities.Team
-    * @example
-    * new Notification(posted).board().id();
-    */
+    //INTERNAL USE ONLY
     this.load = function()
     {
         this.board_list = null;
